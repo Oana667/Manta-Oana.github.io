5a480ff3ed3b8894f61dab736d176547efcc0d9e
Oana667 <91790771+Oana667@users.noreply.github.com>
2025-05-29 08:38:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var typed= new Typed(".typing",{
-    strings:["Curious mind","Team player","Web developer"],
+    strings:["Curious mind","Team player","Web designer","Business analyst","Engineer"],
     typeSpeed:100,BackSpeed:60,loop:true
 })
